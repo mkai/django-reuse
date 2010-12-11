8edde05d7e43b7e9a9cd917d1f510f26e942dc0d
Markus Kaiserswerth <mkai@sensun.org>
2010-12-12 00:05:33 +0100

    Added setuptools information

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='django-reuse',
+      version='0.1',
+      description="General Django adjustments/ utilities library",
+      long_description="",
+      author='Markus Kaiserswerth',
+      author_email='mkai@sensun.org',
+      license='GPL',
+      packages=['reuse'],
+      zip_safe=False,
+      install_requires=['django'],
+      )
