543b9d55f8057531a385403e969fc61a15c3c03e
Markus Kaiserswerth <mkai@sensun.org>
2010-12-16 21:26:23 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-reuse',
-      version='0.1',
+      version='0.11',
       description="General Django adjustments/ utilities library",
       long_description="",
       author='Markus Kaiserswerth',
