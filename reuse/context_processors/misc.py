27ee6ecdf133611eeb0811bde17d2f72844a9433
Markus Kaiserswerth <mkai@sensun.org>
2011-02-21 22:36:08 +0100

    Add exception handling in case request.META['HTTP_HOST'] is not set (why?)

--- reuse/context_processors/misc.py
+++ reuse/context_processors/misc.py
@@ -42,7 +42,10 @@ def full_request_url(request):
     """
     Returns the request's URL in the form http(s)://myhostname.com/my-view/
     """
-    full_path = ''.join(('http', ('', 's')[request.is_secure()], '://', request.META['HTTP_HOST'], request.path))
+    try:
+        full_path = ''.join(('http', ('', 's')[request.is_secure()], '://', request.META['HTTP_HOST'], request.path))
+    except KeyError:
+        full_path = ''
     
     return { 'FULL_REQUEST_URL': full_path }
 
@@ -51,6 +54,9 @@ def full_request_host(request):
     """
     Returns the request's HTTP host in the form http(s)://myhostname.com
     """
-    full_host = ''.join(('http', ('', 's')[request.is_secure()], '://', request.META['HTTP_HOST']))
+    try:
+        full_host = ''.join(('http', ('', 's')[request.is_secure()], '://', request.META['HTTP_HOST']))
+    except KeyError:
+       full_host = '' 
 
     return { 'FULL_REQUEST_HOST': full_host }
