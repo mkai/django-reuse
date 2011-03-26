e4f7514878ac0335ec9e761caa0fa04e4be15d89
Markus Kaiserswerth <mkai@sensun.org>
2011-03-26 19:11:11 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-reuse',
-      version='0.14',
+      version='0.15',
       description="General Django adjustments/ utilities library",
       long_description="",
       author='Markus Kaiserswerth',
