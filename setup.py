1e7fdab7fdf90b2b8f67de179e387e6f1c70389d
Markus Kaiserswerth <mkai@sensun.org>
2011-07-02 00:45:43 +0200

    Added humanized and localized timesince template filter (version bump)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-reuse',
-      version='0.19',
+      version='0.20',
       description="General Django adjustments/ utilities library",
       long_description="",
       author='Markus Kaiserswerth',
