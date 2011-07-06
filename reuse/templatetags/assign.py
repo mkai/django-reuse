f7e8377d403107d5a96b930042f97135c8bfcbd1
Markus Kaiserswerth <mkai@sensun.org>
2011-07-06 22:00:41 +0200

    Remove catch template tag (not working), replace assign template tag with a Django 1.4 version (uses register.assignment_tag)

--- reuse/templatetags/assign.py
+++ reuse/templatetags/assign.py
@@ -1,32 +1,7 @@
-# source: http://djangosnippets.org/snippets/539/
 from django import template
 
-class AssignNode(template.Node):
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-        
-    def render(self, context):
-        context[self.name] = self.value.resolve(context, True)
-        return ''
-
-def do_assign(parser, token):
-    """
-    Assign an expression to a variable in the current context.
-    
-    Syntax::
-        {% assign [name] [value] %}
-    Example::
-        {% assign list entry.get_related %}
-        
-    """
-    bits = token.contents.split()
-    # if len(bits) != 3:
-        # raise template.TemplateSyntaxError("'%s' tag takes two arguments" % bits[0])
-    varname = bits[1]
-    value = " ".join(bits[2:])
-    compiled_value = parser.compile_filter(value)
-    return AssignNode(varname, compiled_value)
-
 register = template.Library()
-register.tag('assign', do_assign)
+
+@register.assignment_tag
+def assign(value):
+    return value
