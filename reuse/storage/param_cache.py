1fdf3b521c2cd8ec763fa388a28b9f2f79a9eca6
Markus Kaiserswerth <mkai@sensun.org>
2011-08-25 17:31:43 +0200

    Add a static files storage class that appends a parameter like '?v=1' to static files in order to invalidate browser cache

---
+++ reuse/storage/param_cache.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.contrib.staticfiles.storage import StaticFilesStorage
+
+CACHE_PARAM = getattr(settings, 'STATICFILES_CACHE_PARAM', '?0')
+
+class ParameterCachedStaticFilesStorage(StaticFilesStorage):
+    def url(self, name):
+        url = super(ParameterCachedStaticFilesStorage, self).url(name)
+        
+        return url + CACHE_PARAM
