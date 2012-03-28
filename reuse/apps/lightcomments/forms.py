b76d015472866a958912e476da6ddde795c0b3f4
Markus Kaiserswerth <github@sensun.org>
2012-03-28 12:19:41 +0200

    Comments app: use timezone-aware timestamps

--- reuse/apps/lightcomments/forms.py
+++ reuse/apps/lightcomments/forms.py
@@ -1,6 +1,6 @@
-import datetime
 from django.conf import settings
 from django.utils.encoding import force_unicode
+from django.utils.timezone import now
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.comments.forms import CommentForm
 
@@ -18,7 +18,7 @@ class LightCommentForm(CommentForm):
             content_type=ContentType.objects.get_for_model(self.target_object),
             object_pk=force_unicode(self.target_object._get_pk_val()),
             comment=self.cleaned_data["comment"],
-            submit_date=datetime.datetime.now(),
+            submit_date=now,
             site_id=settings.SITE_ID,
             is_public=True,
             is_removed=False,
