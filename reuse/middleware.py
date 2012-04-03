dd35b850c76db3e481e89eca8be876eb1f067005
Markus Kaiserswerth <github@sensun.org>
2012-04-03 21:43:54 +0200

    Middleware: PEP8, added DefaultSiteRedirectMiddleware

--- reuse/middleware.py
+++ reuse/middleware.py
@@ -1,11 +1,15 @@
-from django.http import HttpResponse
-from django.conf import settings
 import re
+from django.contrib.sites.models import Site
+from django.http import HttpResponse, HttpResponsePermanentRedirect
+from django.conf import settings
 
 
 class HTTPBasicAuthMiddleware(object):
     """
-    A very basic Basic Auth middleware that uses a username/password defined in your settings.py as BASICAUTH_USERNAME and BASICAUTH_PASSWORD. Does not use Django auth. Handy for quickly securing an entire site during development, for example.
+    A very basic Basic Auth middleware that uses a username/password defined 
+    in your settings.py as BASIC_AUTH_USERNAME and BASIC_AUTH_PASSWORD. 
+    Does not use Django auth. Handy for quickly securing an entire site during
+    development, for example.
 
     In settings.py:
 
@@ -40,6 +44,39 @@ class HTTPBasicAuthMiddleware(object):
             if username == settings.BASIC_AUTH_USERNAME and password == settings.BASIC_AUTH_PASSWORD:
                 return None            
             return self.unauthed()
+
+
+class DefaultSiteRedirectMiddleware(object):
+    """
+    Redirects all requests to a common domain, e. g.: 
+        example.com -> www.example.com
+        example.net -> www.example.com
+        www.example.de -> www.example.com
+
+    The common domain can be specified in a Django setting named SITE_DOMAIN.
+    If SITE_DOMAIN is not set, the domain of the default Site (as in 
+    django.contrib.sites) is used.
+
+    Doesn't do anything during development, i. e. if DEBUG is set to True.
+
+    Adapted from: jezdez http://djangosnippets.org/snippets/989/
+
+    """
+    def process_request(self, request):
+        if settings.DEBUG == True:
+            return None
+
+        domain = getattr(settings, 'SITE_DOMAIN', None)
+        if not domain:
+            domain = Site.objects.get_current().domain
+
+        if domain in request.get_host():
+            return None
+        return HttpResponsePermanentRedirect('%s://%s%s' % (
+            request.is_secure() and 'https' or 'http',
+            domain,
+            request.get_full_path(),
+        ))
 
 
 class StripWhitespaceMiddleware(object):
