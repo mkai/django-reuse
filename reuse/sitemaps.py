2979a408be60ff4f3372b76c0a30a4875b54971e
Markus Kaiserswerth <github@sensun.org>
2012-10-03 18:07:14 +0200

    Added sitemaps module, StaticSitemap

---
+++ reuse/sitemaps.py
@@ -0,0 +1,48 @@
+from django.core import urlresolvers
+from django.conf import settings
+from django.contrib.sitemaps import Sitemap
+import datetime
+import os
+
+
+# from: http://minimoesfuerzo.org/2011/02/12/sitemaps-django-static-pages/
+class StaticSitemap(Sitemap):
+    """Return the static sitemap items"""
+    priority = 0.5
+
+    def __init__(self, patterns, static_url_names=[]):
+        self.patterns = patterns
+        self._items = {}
+        for p in self.patterns:
+            if (getattr(p, 'name', None) is not None and
+                (p.name.startswith('static_') or p.name in static_url_names)):
+                self._items[p.name] = self._get_modification_date(p)
+
+    def _get_modification_date(self, p):
+        template = p.default_args.get('template')
+        if template:
+            template_path = self._get_template_path(template)
+            mtime = os.stat(template_path).st_mtime
+            mod_date = datetime.datetime.fromtimestamp(mtime)
+        else:
+            mod_date = None
+        return mod_date
+
+    def _get_template_path(self, template_path):
+        for template_dir in settings.TEMPLATE_DIRS:
+            path = os.path.join(template_dir, template_path)
+            if os.path.exists(path):
+                return path
+        return None
+
+    def items(self):
+        return self._items.keys()
+
+    def changefreq(self, obj):
+        return 'monthly'
+
+    def lastmod(self, obj):
+        return self._items[obj]
+
+    def location(self, obj):
+        return urlresolvers.reverse(obj)
