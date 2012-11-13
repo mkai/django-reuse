7f7c79ceb115b73a776892a3844b87fe61bdfeac
Markus Kaiserswerth <github@sensun.org>
2012-11-13 18:26:55 +0100

    Added PaginationMixin for ListView subclasses

---
+++ reuse/views.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+
+
+class PaginationMixin(object):
+    """
+    A mixin for Django's ListView that sets project-wide values for pagination
+    behaviour.
+
+    """
+    paginate_by = getattr(settings, 'PAGINATION_OBJECTS_PER_PAGE', 10)
+
+    def get_paginator(self, queryset, *args, **kwargs):
+        per_page = self.paginate_by
+        orphans = getattr(settings, 'PAGINATION_ORPHANS', 0)
+        allow_empty_first_page =\
+            getattr(settings, 'PAGINATION_ALLOW_EMPTY_FIRST_PAGE', True)
+        return super(PaginationMixin, self).get_paginator(queryset, per_page,
+                orphans, allow_empty_first_page)
