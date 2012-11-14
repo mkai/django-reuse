b7e32709f1d21c76693611ce429931d95459ae59
Markus Kaiserswerth <github@sensun.org>
2012-11-14 09:56:11 +0100

    Made usage of timezone.now() more explicit

--- reuse/apps/lightcomments/forms.py
+++ reuse/apps/lightcomments/forms.py
@@ -1,28 +1,23 @@
 from django.conf import settings
 from django.utils.encoding import force_unicode
-from django.utils.timezone import now
+from django.utils import timezone
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.comments.forms import CommentForm
 
 
 class LightCommentForm(CommentForm):
-    """
-    A lighter comment form.
-    
-    """
+    """A comment form with unneeded fields removed."""
     def get_comment_create_data(self):
-        """
-        This needs to be overwritten to remove the fields from the class
-        """
+        """Removes unneeded fields."""
         return dict(
             content_type=ContentType.objects.get_for_model(self.target_object),
             object_pk=force_unicode(self.target_object._get_pk_val()),
             comment=self.cleaned_data["comment"],
-            submit_date=now(),
+            submit_date=timezone.now(),
             site_id=settings.SITE_ID,
             is_public=True,
             is_removed=False,
         )
-    
+
 LightCommentForm.base_fields.pop('email')
 LightCommentForm.base_fields.pop('url')
