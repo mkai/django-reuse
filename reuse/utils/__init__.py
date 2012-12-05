11a7668db43a08ad6935e2b265db4fc89138cdc2
Markus Kaiserswerth <github@sensun.org>
2012-12-05 14:28:27 +0100

    Removed file_from_url utils module, moved into __init__

--- reuse/utils/__init__.py
+++ reuse/utils/__init__.py
@@ -1,6 +1,27 @@
+import os
+import urllib2
+from django.core.files import File
+from django.core.files.temp import NamedTemporaryFile
+from django.utils.hashcompat import md5_constructor
 from django.core.cache.backends.base import (MEMCACHE_MAX_KEY_LENGTH,
                                              default_key_func)
-from django.utils.hashcompat import md5_constructor
+
+
+def file_from_url(url):
+    """Returns a file object from the data downloaded from the given URL."""
+    try:
+        request = urllib2.urlopen(url)
+
+        temp_file = NamedTemporaryFile()
+        temp_file.write(request.read())
+        temp_file.flush()
+
+        file_name = urllib2.unquote(os.path.basename(request.geturl()))
+        file_instance = File(temp_file)
+
+        return file_name, file_instance
+    except:
+        return None
 
 
 def safe_cache_key(key, key_prefix, version):
