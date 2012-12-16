36feda3f4b80a142c43dcc3fe60f421a7fd10d6a
Markus Kaiserswerth <github@sensun.org>
2012-12-16 17:26:36 +0100

    Added regex string replace template filter

---
+++ reuse/templatetags/replace.py
@@ -0,0 +1,27 @@
+import re
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def replace(value, args):
+    """
+    Performs a regular expression search/replace on a string in your template.
+
+    {% load replace %} {{ mystring|replace:"/l(u+)pin/m\1gen" }}
+
+    If: mystring = 'lupin, luuuuuupin, and luuuuuuuuuuuuupin are lè pwn'
+    then it will return: mugen, muuuuuugen, and muuuuuuuuuuuuugen are lè pwn
+
+    The argument is in the following format:
+
+    [delim char]regexp search[delim char]regexp replace
+
+    From: http://djangosnippets.org/snippets/60/
+
+    """
+    args = args.split(args[0])
+    search = args[1]
+    replace = args[2]
+    return re.sub(search, replace, value)
