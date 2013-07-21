cd6f9de7825916b04b736c49d1ee6d92dabd1dd8
Markus Kaiserswerth <github@sensun.org>
2013-07-21 13:50:03 +0200

    -n Revised file_from_url function, use requests

--- reuse/utils/__init__.py
+++ reuse/utils/__init__.py
@@ -1,23 +1,27 @@
 import os
-import urllib2
+import requests
+import urlparse
 from django.core.files import File
 from django.core.files.temp import NamedTemporaryFile
+from django.utils.http import urlunquote
 
 
 def file_from_url(url):
     """Returns a file object from the data downloaded from the given URL."""
     try:
-        request = urllib2.urlopen(url)
+        response = requests.get(url, stream=True)
+        if response.status_code == 204:  # No content
+            return None, None, None
+        path = urlparse.urlsplit(response.url).path
+        name = urlunquote(os.path.basename(path))
+        ctype = response.headers['content-type']
 
+        # create temp file with the downloaded data
         temp_file = NamedTemporaryFile()
-        temp_file.write(request.read())
+        temp_file.write(response.content)
         temp_file.flush()
-
-        file_name = urllib2.unquote(os.path.basename(request.geturl()))
-        file_instance = File(temp_file)
-
-        return file_name, file_instance
-    except:
-        return None
-
-
+        fp = File(temp_file)
+        fp.seek(0)
+    except Exception, e:
+        raise IOError(u'Couldn\'t get file from %s: %r' % (url, e))
+    return name, fp, ctype
