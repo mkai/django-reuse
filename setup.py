aa5e2446411e894e6cb1e5cd10a9615a117f12a0
Markus Kaiserswerth <github@sensun.org>
2013-11-01 14:29:59 +0100

    Revised setup.py, v0.5

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
 
 setup(name='django-reuse',
-      version='0.4',
+      version='0.5',
       description="Yet another collection of components commonly used for Django sites",
       long_description="",
       author='Markus Kaiserswerth',
       author_email='mkai@sensun.org',
-      license='GPL',
+      license='MIT',
       packages=['reuse'],
-      zip_safe=False,
-      install_requires=['django'],
-)
+      zip_safe=True,
+      install_requires=['django'])
