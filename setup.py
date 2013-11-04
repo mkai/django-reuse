3f422de62987f1a2467562d6dfaa8ce859c5bbb3
Markus Kaiserswerth <github@sensun.org>
2013-11-05 00:08:45 +0100

    Revised setup files, added __init__.py versioning

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='django-reuse',
-      version='0.5',
-      description="Yet another collection of components commonly used for Django sites",
-      long_description="",
-      author='Markus Kaiserswerth',
-      author_email='mkai@sensun.org',
-      license='MIT',
-      packages=['reuse'],
-      zip_safe=True,
-      install_requires=['django'])
+setup(
+    name='django-reuse',
+    version='.'.join(map(str, __import__('reuse').__version__)),
+    description='Yet another collection of components commonly used for Django sites',
+    author='Markus Kaiserswerth',
+    author_email='github@sensun.org',
+    url='https://github.com/mkai/django-reuse',
+    packages=find_packages(),
+    install_requires=['django'],
+    classifiers=[
+        'Development Status :: 2 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    license="MIT",
+)
