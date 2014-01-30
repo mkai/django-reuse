131e463194d22a9859ffe4040d3cc900f191aef3
Markus Kaiserswerth <github@sensun.org>
2014-01-30 15:48:16 +0100

    Renamed safe_cache_key to memcache_safe_cache_key

--- reuse/utils/cache.py
+++ reuse/utils/cache.py
@@ -1,9 +1,11 @@
+from __future__ import unicode_literals
+
 import hashlib
 from django.core.cache.backends.base import (MEMCACHE_MAX_KEY_LENGTH,
                                              default_key_func)
 
 
-def safe_cache_key(key, key_prefix, version):
+def memcache_safe_cache_key(key, key_prefix, version):
     """Hashes cache keys that are longer than MEMCACHE_MAX_KEY_LENGTH chars."""
     result = default_key_func(key, key_prefix, version)
     if len(result) > MEMCACHE_MAX_KEY_LENGTH:
