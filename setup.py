3d2751bb870d3822f0f4bc7e2dbe52b3f08f87f1
Markus Kaiserswerth <github@sensun.org>
2014-03-11 15:56:55 +0100

    Revised setuptools config files

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
-from __future__ import unicode_literals
+#!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from __future__ import unicode_literals
+import os
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-reuse',
@@ -9,16 +18,14 @@ setup(
     author='Markus Kaiserswerth',
     author_email='github@sensun.org',
     url='https://github.com/mkai/django-reuse',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests*']),
     install_requires=['django'],
     classifiers=[
         'Development Status :: 2 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Django',
     ],
-    license="MIT",
 )
